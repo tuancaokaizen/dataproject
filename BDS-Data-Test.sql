691b06ab82a7338e4bd54f9afd7214613c727659
tuancaokaizen <124554196+tuancaokaizen@users.noreply.github.com>
2023-03-09 23:51:25 +0700

    Add files via upload

--- BDS-Data-Test.sql
+++ BDS-Data-Test.sql
@@ -8,7 +8,7 @@ from (
     where end_date is not null
 ) s ;
 
-SELECT distinct (t.id), t.name, t.start_date, t.end_date, max(s.maxemp) as maxemp, max(s.dateaq) as date_e
+SELECT top 14 t.id, t.name, t.start_date, t.end_date, max(s.maxemp) as maxemp, min(s.dateaq) as date_e
 from test t
 join (
         select sum(count) over (order by dateaq) as maxemp, dateaq
@@ -21,5 +21,5 @@ join (
         where end_date is not null
         ) s
     ) s on dateaq between start_date and coalesce(end_date, '2023-03-07')
-group by t.id, t.name, t.start_date, t.end_date, s.dateaq
+group by t.id, t.name, t.start_date, t.end_date, s.maxemp
 order by maxemp desc;
